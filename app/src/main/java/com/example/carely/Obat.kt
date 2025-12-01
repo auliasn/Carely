c7d318b127f8b2ff8cdabe5929ada16fded09dcd
nazwaacamila6 <meelahtrash@gmail.com>
2025-12-02 06:09:10 +0700

    commit by nazwa add obat, home masih belum diperbaiki

--- app/src/main/java/com/example/carely/Obat.kt
+++ app/src/main/java/com/example/carely/Obat.kt
@@ -1,14 +1,17 @@
 package com.example.carely
 
-data class Obat (
-    val id : Int,
-    val name : String,
-    val dose : String,
+data class Obat(
+    val id: Int,
+    val name: String,
+    val dose: String,
     val hour: Int,
     val minute: Int,
-    val note : String? = "-",
-    val status : statusObat = statusObat.BELUM_DIMINUM
-)
+    val note: String? = "-",
+    val status: statusObat = statusObat.BELUM_DIMINUM
+) {
+    val time: String
+        get() = "%02d.%02d".format(hour, minute)
+}
 
 enum class statusObat {
     SUDAH_DIMINUM,
