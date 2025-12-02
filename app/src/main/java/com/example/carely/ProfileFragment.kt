b1bc41f378476bf6b6af6bb55aee730f250b202d
Tio-Fernando <fernandotio661@gmail.com>
2025-12-02 09:20:58 +0700

    menambahkan alur logout

--- app/src/main/java/com/example/carely/ProfileFragment.kt
+++ app/src/main/java/com/example/carely/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.example.carely
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 
 class ProfileFragment : Fragment() {
@@ -24,6 +26,13 @@ class ProfileFragment : Fragment() {
         nameField.setText(username ?: "")
         val passField = view.findViewById<EditText>(R.id.etPassword)
         passField.setText(password ?: "")
+
+        val btnLogout = view.findViewById<Button>(R.id.btnLogout)
+        btnLogout.setOnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
 
         return view
     }
